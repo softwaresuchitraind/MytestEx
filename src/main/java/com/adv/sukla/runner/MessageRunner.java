281fad581606410c19db637a9e90d06726d44dda
softwaresuchitraind <software.suchitra@gmail.com>
2021-10-19 08:54:22 +0530

    MessageRunner committed

---
+++ src/main/java/com/adv/sukla/runner/MessageRunner.java
@@ -0,0 +1,18 @@
+package com.adv.sukla.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner
+{
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		System.out.println("DONE");
+
+	}
+
+}
