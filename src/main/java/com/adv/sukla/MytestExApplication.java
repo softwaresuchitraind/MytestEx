fb333547a55a8c6f3978f4022c26683dd7f6214f
softwaresuchitraind <software.suchitra@gmail.com>
2021-10-19 08:33:29 +0530

    first commit

---
+++ src/main/java/com/adv/sukla/MytestExApplication.java
@@ -0,0 +1,13 @@
+package com.adv.sukla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytestExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytestExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adv/sukla/MytestExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adv.sukla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
